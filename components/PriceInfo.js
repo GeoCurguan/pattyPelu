21feca53d71d150dad2db73dc8f1c1fcfb295bfa
Geovanni Curguan <curguangeo@gmail.com>
2022-12-03 03:17:20 -0300

    Comienzo de estelizado del sitio

--- components/PriceInfo.js
+++ components/PriceInfo.js
@@ -19,9 +19,10 @@ const PriceInfo = (props) => {
                 <div class="infoCont">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
-                    <div class="imgCont">
-                        <div class="priceTag"><p>$10000</p></div>
+                    <div class="imgCont" style={{justifyContent:"flex-start"}}>
                         <img src={img}></img>
+                        <div class="priceTag"><p>$10000</p></div>
+
                     </div>
                 </div>
             )}
